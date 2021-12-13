18f3832b127a56e0d5e6997a34636a2d0441a9e9
vened <wlq.mac@gmail.com>
2021-12-13 14:47:17 +0100

    first commit

--- src/components/UserPanel/UserPanel.js
+++ src/components/UserPanel/UserPanel.js
@@ -4,24 +4,7 @@ import Axios from 'axios';
 import UserWeatherItem from './UserWeatherItem';
 import {Link} from 'react-router-dom';
 import { useForm } from 'react-hook-form';
-app.use(function (req, res, next) {
 
-    // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'https://kind-almeida-03b0a9.netlify.app');
-
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', true);
-
-    // Pass to next layer of middleware
-    next();
-});
 function UserPanel()
 {
 
