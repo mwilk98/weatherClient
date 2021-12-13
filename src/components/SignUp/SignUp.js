1d2f13facb3743e1e7d358b2478037c73c208098
vened <wlq.mac@gmail.com>
2021-12-13 14:31:21 +0100

    first commit

--- src/components/SignUp/SignUp.js
+++ src/components/SignUp/SignUp.js
@@ -4,7 +4,24 @@ import {Link} from 'react-router-dom';
 import { useHistory } from "react-router-dom";
 import './SignUp.css';
 import { useForm } from 'react-hook-form';
+app.use(function (req, res, next) {
 
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'https://kind-almeida-03b0a9.netlify.app');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
 function SignUp()
 {
 
